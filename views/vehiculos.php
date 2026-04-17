7d717db7ac2e35ea1a4f6377f942da8b22852c06
danielapinerua <daniela.pinerua@davinci.edu.ar>
2026-04-17 19:10:50 -0300

    probe el metodo static de vehiculo.php

--- views/vehiculos.php
+++ views/vehiculos.php
@@ -15,6 +15,11 @@
     <main>
         <section>
             <h1>Vehiculos</h1>
+            <?php
+require_once('../src/clases/Vehiculo.php'); 
+$vehiculosDisponibles = Vehiculo::getContador();
+?> 
+<p>Hay <?php echo $vehiculosDisponibles; ?> vehículos disponibles.</p>
         </section>
     </main>
     <?php
