7a712caf804bb745497a6cc76e4f55766c0df4a1
danielapinerua <daniela.pinerua@davinci.edu.ar>
2026-04-19 13:21:56 -0300

    subi setters de vehiculo

--- src/clases/Vehiculo.php
+++ src/clases/Vehiculo.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ . '/../../exceptions/VehiculoException.php';  
+
 class Vehiculo {
     private string $marca;
     private string $modelo;
@@ -22,15 +24,15 @@ class Vehiculo {
         int $usuario_id,
         string $transmision
     ) {
-        $this->marca = $marca;
-        $this->modelo = $modelo;
-        $this->anio = $anio;
-        $this->precio = $precio;
-        $this->tipo = $tipo;
-        $this->color = $color;
-        $this->imagen = $imagen;
-        $this->usuario_id = $usuario_id;
-        $this->transmision = $transmision;
+        $this->setMarca($marca);
+        $this->setModelo($modelo);
+        $this->setAnio($anio);
+        $this->setPrecio($precio);
+        $this->setTipo($tipo);
+        $this->setColor($color);
+        $this->setImagen($imagen);
+        $this->setUsuarioId($usuario_id);
+        $this->setTransmision($transmision);
     }
 
        public function getMarca() {
@@ -59,7 +61,6 @@ class Vehiculo {
     }
 
     public function getColor() {
-
         return $this->color;
 
     }
@@ -78,6 +79,79 @@ class Vehiculo {
         return $this->transmision;
 
     }
+    public function setMarca(string $marca): void {
+        if (!empty($marca)) {
+            $this->marca = $marca;
+        } else {
+            throw new VehiculoException("Marca inválida");
+        }
+    }
+
+    public function setModelo(string $modelo): void {
+        if (!empty($modelo)) {
+            $this->modelo = $modelo;
+        } else {
+            throw new VehiculoException("Modelo inválido");
+        }
+    }
+
+    public function setAnio(int $anio): void {
+        $anioActual = date("Y");
+        if ($anio >= 1886 && $anio <= $anioActual) {
+            $this->anio = $anio;
+        } else {
+            throw new VehiculoException("Año inválido");
+        }
+    }
+
+    public function setPrecio(float $precio): void {
+        if ($precio > 0) {
+            $this->precio = $precio;
+        } else {
+            throw new VehiculoException("Precio inválido");
+        }
+    }
+
+    public function setTipo(string $tipo): void {
+        if (!empty($tipo)) {
+            $this->tipo = $tipo;
+        } else {
+            throw new VehiculoException("Tipo inválido");
+        }
+    }
+
+    public function setColor(string $color): void {
+        if (!empty($color)) {
+            $this->color = $color;
+        } else {
+            throw new VehiculoException("Color inválido");
+        }
+    }
+
+    public function setImagen(string $imagen): void {
+        if (!empty($imagen)) {
+            $this->imagen = $imagen;
+        } else {
+            throw new VehiculoException("Imagen inválida");
+        }
+    }
+
+    public function setUsuarioId(int $usuario_id): void {
+        if ($usuario_id > 0) {
+            $this->usuario_id = $usuario_id;
+        } else {
+            throw new VehiculoException("Usuario inválido");
+        }
+    }
+
+    public function setTransmision(string $transmision): void {
+        if (!empty($transmision)) {
+            $this->transmision = $transmision;
+        } else {
+            throw new VehiculoException("Transmisión inválida");
+        }
+    }
+
 
 
     
